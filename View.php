3ec686c5fe7030f449a5e11e1129c0daafab836a
Abdessalam Bahafid <zomiix246@gmail.com>
2021-09-06 02:34:39 +0100

    add support in our blade to: foreach, if-else,while fix bug in creating cache directories

--- View.php
+++ View.php
@@ -19,8 +19,17 @@ class View {
         // change {{$var}} => <?=$var?\> # yeah, you spelled it write
         $content = preg_replace("{{{([^\}]*)}}}", "<?=$1?>", $content);
 
+        // change @foreach($data as $d) ... @endforeach => <?php foreach($data as $d) ?\> ... <?php endforeach ?\>
+        // same for: @if @else @endif
+        // same as: @for(...) @endfor, @while(...) @endwhile
+        $content = preg_replace("#@(\w+)(\(.*\))#", "<?php $1$2{ ?>", $content);
+        $content = preg_replace("#@else#", "<?php }else{ ?>", $content);
+        $content = preg_replace("#\@(\w+)[^\(]#", "<?php } ?>", $content);
+
         try{
             $final_path_to_views = str_replace("views", "cache", $path_to_views);
+            if (!is_dir($dirname = preg_replace("#[^/]\w+\.php$#", "", $final_path_to_views)))
+                mkdir($dirname, 0755, true);
             $file = fopen($final_path_to_views, "w+");
             fwrite($file, $content);
             fclose($file);
