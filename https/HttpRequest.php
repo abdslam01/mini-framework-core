833985cfa6cde2d4351caede44de08fe3708a771
Abdessalam Bahafid <zomiix246@gmail.com>
2021-09-08 13:45:14 +0100

    add getBody method to HttpRequest class

--- https/HttpRequest.php
+++ https/HttpRequest.php
@@ -5,6 +5,21 @@ namespace Abdslam01\MiniFrameworkCore\Https;
 /**
  * HttpRequest
  */
-class HttpRequest {
-    
+class HttpRequest {    
+    /**
+     * getBody
+     *
+     * @return array
+     */
+    public function getBody(): array{
+        $body = [];
+        if($_SERVER['REQUEST_METHOD'] === 'GET'){
+            foreach($_GET as $key=>$_)
+                $body[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+        }else{
+            foreach($_POST as $key=>$_)
+                $body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+        }
+        return $body;
+    }
 }
